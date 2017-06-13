f19c46c1b9ee27f6ba866c5d958f871718783045
Michael Chary <mac389@gmail.com>
2017-06-13 07:36:30 -0400

    new Twitter api

--- src/2017/acuire-tweets.py
+++ src/2017/acuire-tweets.py
@@ -1,6 +1,25 @@
-import json 
+import json, datetime
+
 
 from TwitterAPI import TwitterAPI
 
-keys = json.load(open(''))
-api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+directory = json.load(open('../../data/directory.json','rb'))
+keys = directory['credentials']
+
+api = TwitterAPI(keys['consumer_key'], keys['consumer_secret'], 
+				keys['access_token'], keys['access_token_secret'])
+
+query = 'aids'
+timestamp = '%Y-%m-%d %H:%M:%S'.format(datetime.datetime.now())
+outfile = '../../data/%s-%s'%(query,timestamp)
+r = api.request('search/tweets', {'q':'aids'})
+
+with open(outfile,'wb') as outfile:
+	for item in r:
+        print(item)
+        json.dump(item,outfile)
+
+
+r = api.request('statuses/filter', {'locations':'-74,40,-73,41'})
+for item in r:
+        print(item)
